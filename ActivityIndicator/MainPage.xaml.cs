3bc681c68221e655c1ba2848461db3110a008081
Samuel Juren <sammjureen@outlook.com>
2024-05-14 21:20:09 -0300

    Add project files.

---
+++ ActivityIndicator/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ActivityIndicator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void webView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            WebViewVisibility(true);
+        }
+
+        private void webView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            WebViewVisibility(false);
+        }
+
+        private void WebViewVisibility(bool isWebViewVisible)
+        {
+            webView.IsVisible = isWebViewVisible;
+            actIndicator.IsVisible = !isWebViewVisible;
+            actIndicator.IsRunning = !isWebViewVisible;
+        }
+    }
+}
